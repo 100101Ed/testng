80d79488926b089116dd04c192fa6d6c9d2c8010
100101Ed <edm100101@hotmail.com>
2016-02-11 20:04:10 -0800

    Implement Dispay Matrix Object

--- src/main/java/com/sqa/em/util/helper/IdentifyObject.java
+++ src/main/java/com/sqa/em/util/helper/IdentifyObject.java
@@ -9,6 +9,11 @@
  */
 package com.sqa.em.util.helper;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * IdentifyObject //ADDD (description of class)
  * <p>
@@ -22,27 +27,105 @@ package com.sqa.em.util.helper;
  */
 public class IdentifyObject {
 
-	// Methods which call Object methods getClass().getSimpleName(), toString(),
-	// and in a readable form both to display and get String value of Object
+	// Uses Reflection API
 	public static void display2DInfo(Object[][] objects) {
+		System.out.println(get2DInfo(objects));
 	}
 
+	// Use this method to get info about the class being passed
+	// Uses Reflection API
 	public static void displayInfo(Object obj) {
-		System.out.println("Name (Package + Class): " + obj.getClass().getName());
-		System.out.println("Package name: " + obj.getClass());
-		// System.out.println(obj.toString());
-		System.out.println("Class name: " + getInfo(obj));
-		// System.out.println(obj.getClass());
+		System.out.println("Object Type: " + obj.getClass().getSimpleName());
+		System.out.print("\tFields are: " + Arrays.toString(getFieldNames(obj)) + "\n");
+		System.out.print("\tMethods are: " + Arrays.toString(getMethodNames(obj)) + "\n");
 	}
 
+	// Uses Reflection API
 	public static void displayInfo(Object obj1, Object obj2, Object... objN) {
 	}
 
-	public static String get2DInfo(Object[][] objects) {
-		return "";
+	//////// 2DMatrix
+	public static void displayInfo(Object[] obj) {
+		for (Object object : obj) {
+			displayInfo(object);
+		}
 	}
 
-	public static String getInfo(Object obj) {
-		return obj.getClass().getSimpleName();
+	//////// 2DMatrix
+	public static String get2DInfo(Object[][] objects) {
+		StringBuilder sBuilder = new StringBuilder();
+		sBuilder.append("Display 2D Matrix\n\n");
+		for (int row = 0; row < objects.length; row++) {
+			for (int colum = 0; colum < objects.length; colum++) {
+				sBuilder.append("\t" + objects[row][colum]);
+			}
+			sBuilder.append("\n");
+		}
+		return sBuilder.toString();
 	}
+
+	// Uses Reflection API
+	public static String[] getFieldNames(Object obj) {
+		String[] names;
+		ArrayList<String> fieldNames = new ArrayList<String>();
+		Field[] fields = obj.getClass().getFields();
+		for (Field field : fields) {
+			fieldNames.add(field.getName());
+		}
+		names = new String[fieldNames.size()];
+		fieldNames.toArray(names);
+		return names;
+	}
+
+	// Uses Reflection API
+	public static String getInfo(Object obj) {
+		return null;
+	}
+
+	// Uses Reflection API
+	public static String[] getMethodNames(Object obj) {
+		String[] names;
+		ArrayList<String> methodNames = new ArrayList<String>();
+		Method[] methods = obj.getClass().getMethods();
+		for (Method method : methods) {
+			methodNames.add(method.getName());
+		}
+		names = new String[methodNames.size()];
+		methodNames.toArray(names);
+		return names;
+	}
+	/*
+	 * // Methods which call Object methods getClass().getSimpleName(),
+	 * toString(), // and in a readable form both to display and get String
+	 * value of Object public static void display2Array(Object[][] objects) {
+	 * System.out.println(get2DInfo(objects)); } public static void
+	 * displayInfo(Object obj) { System.out.println("This object is named: " +
+	 * obj.getClass().getName()); System.out.println("\tThe object is of type: "
+	 * + obj.getClass().getSimpleName()); System.out.println(
+	 * "\tThe number of modifiers are: " + obj.getClass().getModifiers());
+	 * System.out.println("\tThe fields are: " +
+	 * Arrays.deepToString(obj.getClass().getFields())); System.out.println(
+	 * "\tThe methods are: " +
+	 * Arrays.deepToString(obj.getClass().getDeclaredMethods())); } public
+	 * static void displayInfo(Object obj1, Object obj2, Object... objN) {
+	 * displayInfo(obj1); displayInfo(obj2); for (Object object : objN) {
+	 * displayInfo(object); } } public static void displayInfo(Object[] obj1) {
+	 * for (Object object : obj1) { displayInfo(object); } } public static
+	 * String get2DInfo(Object[][] objects) { StringBuilder builder = new
+	 * StringBuilder(); for (Object[] objects2 : objects) { for (Object object :
+	 * objects2) { builder.append("\""); builder.append(object.toString());
+	 * builder.append("\"\t"); } builder.append("\n"); } return
+	 * builder.toString(); } public static String getInfo(Object obj) {
+	 * StringBuilder builder = new StringBuilder(); builder.append(
+	 * "[ This object is named = "); builder.append(obj.getClass().getName());
+	 * builder.append(", The object is of type = ");
+	 * builder.append(obj.getClass().getSimpleName()); builder.append(
+	 * ", tThe number of modifiers are = ");
+	 * builder.append(obj.getClass().getModifiers()); builder.append(
+	 * ", tThe fields are = ");
+	 * builder.append(Arrays.deepToString(obj.getClass().getFields()));
+	 * builder.append(", The methods are = ");
+	 * builder.append(Arrays.deepToString(obj.getClass().getDeclaredMethods()));
+	 * builder.append(" ]"); return builder.toString(); }
+	 */
 }
